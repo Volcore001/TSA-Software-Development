850e12c1143ddf05a43275e8fc65bc9d926aafd0
Leon Li <31671708+leonli33@users.noreply.github.com>
2018-12-29 16:19:21 -0800

    Added 3 buttons

--- app/src/main/java/com/leon/tsasoftware/MainActivity.java
+++ app/src/main/java/com/leon/tsasoftware/MainActivity.java
@@ -1,13 +1,34 @@
 package com.leon.tsasoftware;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button SearchAuthors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
+
+        SearchAuthors = findViewById(R.id.ButtonSearch);
+
+
+        SearchAuthors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent newIntent = new Intent(MainActivity.this,Search.class);
+                startActivity(newIntent);
+            }
+        });
+
+
     }
+
 }
